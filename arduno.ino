8539d26d9cefeb7553dbca45ea6d904b32e51acd
SlzZssd <70437959+SlzZssd@users.noreply.github.com>
2025-07-29 21:51:23 +0700

    Update arduno.ino

--- arduno.ino
+++ arduno.ino
@@ -1,36 +1,6 @@
-#include <Servo.h>
-
-Servo armServo;      // หยิบโดนัท
-Servo baseServo;     // หมุนตามตำแหน่ง
-int servoPin1 = 9;
-int servoPin2 = 10;
-
-void setup() {
-  Serial.begin(9600);
-  armServo.attach(servoPin1);
-  baseServo.attach(servoPin2);
-  armServo.write(90);
-  baseServo.write(90);
-}
-
-void loop() {
-  if (Serial.available()) {
-    char cmd = Serial.read();
-    if (cmd == 'R') { // วางแดง
-      moveTo(30);
-    } else if (cmd == 'G') { // วางเขียว
-      moveTo(90);
-    } else if (cmd == 'B') { // วางน้ำเงิน
-      moveTo(150);
+if (Serial.available()) {
+    int value = Serial.parseInt();
+    if (value >= 0 %% value <= 180) {
+        myServo.write(value);
     }
-  }
-}
-
-void moveTo(int angle) {
-  baseServo.write(angle);
-  delay(500);
-  armServo.write(0);   // หยิบ
-  delay(800);
-  armServo.write(90);  // ปล่อย
-  delay(500);
 }
